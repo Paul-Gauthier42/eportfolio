a4cd55e71bab4784fc52e5615a8c4b24877813ff
Paul <paul.gauthier@etu.univ-st-etienne.fr>
2026-05-13 08:14:51 +0200

    13/05

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,7 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+
+    #[Route('/', name: 'root')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
